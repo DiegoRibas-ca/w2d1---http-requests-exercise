d19b735d37579b5e3439d05a6117e2f6839e594a
DiegoRibas-ca <diegoribas.ca@gmail.com>
2018-01-22 18:00:29 +0000

    export and require files created

---
+++ http-functions.js
@@ -0,0 +1,26 @@
+const https = require('https');
+module.exports = function getHTML (options, callback) {
+    /* Your code here */
+  const https = require('https');
+
+  /* Add your code here */
+  https.get(options, function (response) {
+    var allChunkJoin = '';
+    response.setEncoding('utf8');
+
+    response.on('data', function(data){
+      allChunkJoin += data;
+    });
+
+    response.on('end', function(){
+      print(allChunkJoin);
+      console.log("All response in an unique output");
+    });
+  });
+
+  var print = function printHTML (html) {
+    console.log(html);
+  }
+
+
+};
